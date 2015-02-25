2c6b2aaafd650c2e36810d3324b5b4a6099ff15f
Kosy <kosyfrances@yahoo.com>
2015-02-25 01:16:14 +0100

    Added list of artist on html

--- scripts/track-finder.js
+++ scripts/track-finder.js
@@ -1,30 +1,44 @@
 var trackFinder = {
   initialize: function() {
-    console.log("initialized");
     this.search();
+    $("#error").hide();
   },
   search: function() {
     $("#search").click(function() {
+      event.preventDefault(); 
+       $("#result").empty();
+       $("#error").hide();
       var input = $("#textBox").val().trim();
+    
 
       //Checks for spaces and replaces them with a hyphen
       for (var i = 0; i < input.length; i++) {
         input = input.replace(" ", "-");
       }          
 
-      var url = "https://api.soundcloud.com/tracks/"+ input + "?client_id=dfe6c7a24917b01e30400444842a6de4";
+      var url = "https://api.soundcloud.com/tracks?q="+ input + "&client_id=dfe6c7a24917b01e30400444842a6de4";
       $.getJSON(url, function(reply) {       
-        var streamUrl =  reply.stream_url + "?client_id=dfe6c7a24917b01e30400444842a6de4"; 
-        console.log("main stream url: ", streamUrl);
-        console.log("response: ",reply); 
-        console.log("artwork url: ", reply.artwork_url);
-        console.log("Downloadable: ", reply.downloadable);
-        console.log("Title: ", reply.title);
-        console.log("Stream url: ", reply.stream_url);
-        $("#audio").append('<source src="' + streamUrl + '"type="audio/mpeg">');
+      
+        for (var i = 0; i < reply.length; i++) {
+          //check if song is streamable. If it is not, media player won't play it
+          if (reply[i].streamable === true) {
+            var streamUrl = reply[i].stream_url + "?client_id=dfe6c7a24917b01e30400444842a6de4",
+                title = reply[i].title,
+                artworkUrl = reply[i].artwork_url;
+
+            $("#result").append("<ul>" + "<li>" +  title + "</li>" + "<li>" + artworkUrl + "</li>" + "</ul>");          
+            $("#audio").append('<source src="' + streamUrl + '"type="audio/mpeg">');        
+          }
+        }
+        // if ($("#result").html === "") {
+        //   $("#error").html("No search found");
+        //   $("#result").hide();
+        // }
+        
       });
     });    
   }
 }
 $(document).ready(trackFinder.initialize())
+
 
